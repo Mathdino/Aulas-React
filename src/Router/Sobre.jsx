4dffff52bd2fb7d165cbd91e8b3b532022dc5611
Matheus Vinicius Moraes Bernardino <matheusvmb@educ.guarulhos.gov>
2025-03-20 17:18:57 -0300

    Mais sobre routers

--- src/Router/Sobre.jsx
+++ src/Router/Sobre.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Head from './Head';
 
 const Sobre = () => {
   return (
     <div>
+      <Head title="Sobre" description="Essa é a descrição da Sobre" />
       <h1>SOBRE</h1>
       <p>Essa é o sobre</p>
     </div>
